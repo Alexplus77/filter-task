9d53714a8b896fd759b253af4ca7385a216c7e94
Alexplus77 <alexplus77@mail.ru>
2021-10-13 10:18:41 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import React from "react";
 import Portfolio from "./Components/portfolio";
 
-const App=()=> <Portfolio/>;
+const App = () => (
+  <div>
+    <Portfolio />
+  </div>
+);
 
 export default App;
